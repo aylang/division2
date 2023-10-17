b01aa44acdcec661a320a81527e1e0ca252ff3b6
Professional <alang@mail.ru>
2023-10-18 03:07:30 +0500

    fix

--- src/split-to-divider.spec.ts
+++ src/split-to-divider.spec.ts
@@ -1,30 +1,30 @@
-import {splitToDivider} from "./split-to-divider";
+import {splitToDivider, splitToDivider12, splitToDivider13} from "./split-to-divider";
+import {primeNumbers} from "./prime-numbers";
 
 describe('Разделить число на делители', () => {
-    it('Наименьшие положительные больше единицы.', () => {
-        // expect(splitToDivider(2)).toEqual([2,1]);
-        // expect(splitToDivider(3)).toEqual([3,1]);
-        // expect(splitToDivider(4)).toEqual([2,2,1]);
-        // expect(splitToDivider(5)).toEqual([5,1]);
-        // expect(splitToDivider(6)).toEqual([2,3,1]);
-        // expect(splitToDivider(7)).toEqual([7,1]);
-        // expect(splitToDivider(8)).toEqual([2,2,2,1]);
-        // expect(splitToDivider(9)).toEqual([3,3,1]);
-        // expect(splitToDivider(10)).toEqual([2,5,1]);
-        // expect(splitToDivider(11)).toEqual([11,1]);
-        // expect(splitToDivider(12)).toEqual([2,2,3,1]);
-        // expect(splitToDivider(13)).toEqual([13,1]);
-        // expect(splitToDivider(14)).toEqual([2,7,1]);
-        // expect(splitToDivider(15)).toEqual([3,5,1]);
-        // expect(splitToDivider(16)).toEqual([2,2,2,2,1]);
-        // expect(splitToDivider(17)).toEqual([17,1]);
-        // expect(splitToDivider(18)).toEqual([2,3,3,1]);
-        // expect(splitToDivider(19)).toEqual([19,1]);
-        // expect(splitToDivider(20)).toEqual([2,2,5,1]);
-        // expect(splitToDivider(21)).toEqual([3,7,1]);
-        // expect(splitToDivider(98)).toEqual([2,11,1]);
 
-        console.log('841: ',splitToDivider(1000000))
+    it('Наименьшие положительные больше единицы.', () => {
+        expect(splitToDivider(2)).toEqual([2,1]);
+        expect(splitToDivider(3)).toEqual([3,1]);
+        expect(splitToDivider(4)).toEqual([2,2,1]);
+        expect(splitToDivider(5)).toEqual([5,1]);
+        expect(splitToDivider(6)).toEqual([2,3,1]);
+        expect(splitToDivider(7)).toEqual([7,1]);
+        expect(splitToDivider(8)).toEqual([2,2,2,1]);
+        expect(splitToDivider(9)).toEqual([3,3,1]);
+        expect(splitToDivider(10)).toEqual([2,5,1]);
+        expect(splitToDivider(11)).toEqual([11,1]);
+        expect(splitToDivider(12)).toEqual([2,2,3,1]);
+        expect(splitToDivider(13)).toEqual([13,1]);
+        expect(splitToDivider(14)).toEqual([2,7,1]);
+        expect(splitToDivider(15)).toEqual([3,5,1]);
+        expect(splitToDivider(16)).toEqual([2,2,2,2,1]);
+        expect(splitToDivider(17)).toEqual([17,1]);
+        expect(splitToDivider(18)).toEqual([2,3,3,1]);
+        expect(splitToDivider(19)).toEqual([19,1]);
+        expect(splitToDivider(20)).toEqual([2,2,5,1]);
+        expect(splitToDivider(21)).toEqual([3,7,1]);
+        expect(splitToDivider(98)).toEqual([2,7,7,1]);
     });
 
     it('Частота применения признака делимости на чесло', () => {
@@ -35,7 +35,7 @@ describe('Разделить число на делители', () => {
             // а предпоследнее простое число,
             // которое сравниваю с таблицей простых чисел,
             // те не подбираю по признаку делимости.
-            for (let j = 0; j < (split.length - 2); j++) {
+            for (let j = 0; j < (split.length - 1); j++) {
                 map.set(split[j], (map.get(split[j]) ?? 0) + 1)
             }
         }
@@ -52,9 +52,13 @@ describe('Разделить число на делители', () => {
         }
         console.log(result)
     })
+});
+
+describe('Тест производительности больших чисел', () => {
+    const tested = 100000000;
+    const prime = primeNumbers(tested);
 
     it('Подбор делителей методом перебора', () => {
-        const tested = 1000000;
         const result = [];
         for (let i = 1; i <= tested; i++) {
             if (tested % i === 0) {
@@ -63,4 +67,9 @@ describe('Разделить число на делители', () => {
         }
         console.log(result)
     })
-});
+
+
+    it('Подбор делителей функцией', () => {
+        console.log(splitToDivider13(tested, prime))
+    })
+})
--- src/split-to-divider.ts
+++ src/split-to-divider.ts
@@ -2,6 +2,59 @@
 import {primeNumbers} from "./prime-numbers";
 import {isDivided2, isDivided3, isDivided5, isDivided7, isDivided9} from "./sign-of-division";
 
+
+export function splitToDivider13(value: number, prime: number[]): number[] {
+    const result = [];
+    let remains = value;
+    while (remains !== 1) {
+        for (const primeNumber of prime) {
+            if (remains % primeNumber === 0) {
+                result.push(primeNumber);
+                remains = remains / primeNumber;
+                break;
+            }
+        }
+    }
+    result.push(1);
+    return result;
+}
+export function splitToDivider12(value: number, prime: number[]): number[] {
+    const result = [];
+    let remains = value;
+    while (!prime.includes(remains)) {
+        for (const primeNumber of prime) {
+            if (remains % primeNumber === 0) {
+                result.push(primeNumber);
+                remains = remains / primeNumber;
+                break;
+            }
+        }
+    }
+    result.push(remains, 1);
+    return result;
+}
+export function splitToDivider11(value: number): number[] {
+    const prime = primeNumbers(value);
+    const result = find(value);
+    return result
+
+    function find(value: number): number[] {
+        const result = [];
+        let remains = value;
+        while (!prime.includes(remains)) {
+            for (const primeNumber of prime) {
+                if (remains % primeNumber === 0) {
+                    result.push(primeNumber);
+                    remains = remains / primeNumber;
+                    break;
+                }
+            }
+        }
+        result.push(remains, 1);
+        return result;
+    }
+}
+
 export function splitToDivider(value: number): number[] {
     const prime = primeNumbers(value);
     const result = find(value);
